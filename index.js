414549d6dafc68e36b94549719e6fc774acf3f53
franciscoha <54969936+franciscoha@users.noreply.github.com>
2020-03-29 21:18:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var socket = require('socket.io');
 // App setup
 var app = express();
 var server = app.listen(3000, function(){
-    console.log('listening for requests on port 4000,');
+    console.log('listening for requests on port 3000');
 });
 
 // Static files
-app.use(express.static('client'));
+app.use(express.static('public'));
 
 // Socket setup & pass server
 var io = socket(server);
