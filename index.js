ed0117a02761ea9a35258a8ea5467472a84621af
franciscoha <54969936+franciscoha@users.noreply.github.com>
2020-03-29 21:49:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+
+
 var express = require('express');
-var server = http.Server(app);
 var app = express();
-var http = require('http');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);    
+var port = process.env.PORT || 3000;
 
-var PORT = process.env.PORT || 5000;
-
-app.use(express.static('client'));
-
-server.listen(PORT, function() {
-  console.log('Chat server running');
+app.use(express.static('public'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-var io = require('socket.io')(server);
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id);
 
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+  
 });
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
